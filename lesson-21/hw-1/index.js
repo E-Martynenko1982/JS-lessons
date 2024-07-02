6b9676c0eb4f5347bcc728dc15a1a5b6aac178cd
E-Martynenko1982 <motor8103@gmail.com>
2024-07-02 22:01:34 +0300

    hw-1

--- lesson-21/hw-1/index.js
+++ lesson-21/hw-1/index.js
@@ -1,13 +1,14 @@
 export const getTitleElement = () => {
   const titleElem = document.querySelector('.title')
-  return console.dir(titleElem)
+  console.dir(titleElem)
+  return titleElem
 };
 getTitleElement()
 
 export const getInputElement = () => {
   const inputElem = document.querySelector('input[type="text"]')
-  return console.dir(inputElem)
-
+  console.dir(inputElem)
+  return inputElem
 };
 
 getInputElement()
