7b35682fa08ce3826d022926132a92de7ca6bc1f
E-Martynenko1982 <motor8103@gmail.com>
2024-07-02 21:58:44 +0300

    hw-1

---
+++ lesson-21/hw-1/index.js
@@ -0,0 +1,13 @@
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title')
+  return console.dir(titleElem)
+};
+getTitleElement()
+
+const getInputElement = () => {
+  const inputElem = document.querySelector('input[type="text"]')
+  return console.dir(inputElem)
+
+};
+
+getInputElement()
