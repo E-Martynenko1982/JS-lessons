06024b77ad5e856d54841d712cc7393017c8dc71
E-Martynenko1982 <motor8103@gmail.com>
2024-07-03 11:57:22 +0300

    hw7

---
+++ lesson-21/hw-7/index.js
@@ -0,0 +1,7 @@
+export const createButton = (buttonText) => {
+  const bodyElem = document.querySelector('body')
+  const buttonElem = document.createElement("button");
+  buttonElem.textContent = buttonText;
+  bodyElem.appendChild(buttonElem)
+};
+createButton('Play')
