72874bf3f3da95168c8cf08299c29c0fee8421d1
E-Martynenko1982 <motor8103@gmail.com>
2024-07-03 10:13:01 +0300

    hw3

--- lesson-21/hw-3/index.js
+++ lesson-21/hw-3/index.js
@@ -1,12 +1,13 @@
 
-const getTitle = () => document.querySelector('.title').textContent
-console.dir(getTitle())
-
+const getTitle = () => {
+  const result = document.querySelector('.title')
+  return result
+}
 const getDescription = () => document.querySelector('.about').innerText
-console.dir(getDescription())
+
 
 const getPlans = () => document.querySelector('.plans').innerHTML
 
 const getGoal = () => document.querySelector('.goal').outerHTML
-console.dir(getGoal())
+
 
