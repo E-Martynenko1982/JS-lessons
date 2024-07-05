61fff267a7ff634d3322359c42a5a6d006a17fd9
Evgeniy Martynenko <84075901+E-Martynenko1982@users.noreply.github.com>
2024-07-05 23:32:11 +0300

    Update index.js

--- lesson-23/hw-1/index.js
+++ lesson-23/hw-1/index.js
@@ -1,69 +1,67 @@
 
-// Цей масив використовується зберігання всіх завдань. Кожне завдання представлене об'єктом із полями
-// 'id', 'text' 'done'
+
 const tasks = [];
 
-// Функція renderTasks приймає масив завдань (tasksList) та відображає їх у вигляді списку на сторінці.
 
 const renderTasks = tasksList => {
   const listElem = document.querySelector(".list");
-  listElem.innerHTML = "";// очистка списку перед повторним рендерингом
+  listElem.innerHTML = "";
 
   const listItemsElems = tasksList
-    //сортування - невиконані таски ідуть перші
+  
     .sort((a, b) => a.done - b.done)
-    // створює елементи списку для кожного завдання
+ 
     .map(({ id, text, done }) => {
-      // створюємо елементи списку і додаємо для них класи з перевіркою якщо статус done-true то навішуємо інший класс для елементів що зроблені(зачеркнуті)
+     
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       if (done === true) {
         listItemElem.classList.add('list__item_done');
       }
-      // створення і налаштування чекбокса
+      
       const checkboxElem = document.createElement('input');
       checkboxElem.setAttribute('type', 'checkbox');
       checkboxElem.checked = done;
       checkboxElem.dataset.taskId = id;
       checkboxElem.classList.add('list__item-checkbox');
       checkboxElem.addEventListener('click', onCheckboxClick);
-      // додавання чекбокса і тексту задачі в елемент списку
+     
       listItemElem.append(checkboxElem, text);
 
       return listItemElem;
     });
-  // додає всі створені елементи до списку на сторінці.
+  
   listElem.append(...listItemsElems);
 };
-// Ця функція перемикає стан завдання (виконана/невиконана) при натисканні на чекбокс
+
 const onCheckboxClick = event => {
-  const taskId = event.target.dataset.taskId; // отримання ід задачі
-  const task = tasks.find(task => task.id === taskId);// пошук задачі по ід
-  task.done = !task.done; // перемикання стану задачі
-  renderTasks(tasks) //повторне оновлення задач
+  const taskId = event.target.dataset.taskId; 
+  const task = tasks.find(task => task.id === taskId);
+  task.done = !task.done; 
+  renderTasks(tasks) 
 };
 
-// Ця функція створює нове завдання і додає її до масиву завдань при натисканні на кнопку "Create".
+
 const createTask = () => {
   const taskInputElem = document.querySelector('.task-input');
-  const text = taskInputElem.value.trim();// отримання значень вводу без пробілів
+  const text = taskInputElem.value.trim();
 
-  if (!text) return;// перевірка що поле не пусте
+  if (!text) return;
 
-  const newTask = {// створення нової задачі
+  const newTask = {
     id: String(Date.now()),// замість ід  - поточна дата
     text,
     done: false,
   }
 
-  tasks.push(newTask);// додавання задачі в масив
-  taskInputElem.value = '' // очистка поля вводу
-  renderTasks(tasks) // повторний рендеринг списку
+  tasks.push(newTask);
+  taskInputElem.value = '' 
+  renderTasks(tasks)
 };
 
-// Додавання обробника події на кнопку Create. При натисканні на кнопку викликається функція createTask.
+
 const createBtnElem = document.querySelector('.create-task-btn');
 createBtnElem.addEventListener('click', createTask);//
 
-// Виклик функції renderTasks для початкового рендерингу списку завдань, навіть якщо він порожній
+
 renderTasks(tasks)
