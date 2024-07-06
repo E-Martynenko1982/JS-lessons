c64e26f35ae33d5531f4970753cbab7ba0fed449
E-Martynenko1982 <motor8103@gmail.com>
2024-07-06 16:31:29 +0300

    update

---
+++ lesson-24/task1/index.js
@@ -0,0 +1,8 @@
+
+const dayOfWeek = (date, days) => {
+  const weekDays = ["Su", "Mo", "Thu", "We", "Th", "Fr", "St",];
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
---
+++ lesson-24/video/index.js
@@ -0,0 +1,25 @@
+const weekDays = ['Sun', "Mon", "Thu", "Wed",]
+const getDayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+
+const result = getDayOfWeek(new Date(2019, 0, 1), 14);
+
+console.log(result);
+
+
+// ----------------------------------
+
+const formatter = new Intl.DateTimeFormat("en", {
+  timeZone: "UTC",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
+
+})
+
+const getTime = date => formatter.format(date);
+
+console.log(getTime(new Date()));
