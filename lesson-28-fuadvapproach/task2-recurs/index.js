027647170343285305e1874b7013a0fc3fab7c03
E-Martynenko1982 <motor8103@gmail.com>
2024-07-10 11:47:29 +0300

    update

--- lesson-28-fuadvapproach/task2-recurs/index.js
+++ lesson-28-fuadvapproach/task2-recurs/index.js
@@ -6,3 +6,19 @@ export const markFavorites = (tree, favorites) => {
     nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
   }
 };
+
+const favorites = ['id-2']
+
+const tree = {
+  id: 'id-1',
+  name: 'Products',
+  nodes: [
+    {
+      id: "id-2",
+      name: 'Food',
+      nodes: [],
+    }
+  ]
+}
+
+markFavorites(tree, favorites)
