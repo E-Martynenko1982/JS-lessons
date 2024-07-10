d3e1f678f736d4ba0c12e0c9d33388908ca66f90
E-Martynenko1982 <motor8103@gmail.com>
2024-07-10 09:52:28 +0300

    update

--- lesson-27-local-storage/task3/src/scripts/createTask.js
+++ lesson-27-local-storage/task3/src/scripts/createTask.js
@@ -3,16 +3,19 @@ import { getItem, setItem } from './storage.js';
 export const createTask = () => {
   const taskInputElem = document.querySelector('.task-input');
   const text = taskInputElem.value.trim();
+
   if (!text) return;
+
   taskInputElem.value = '';
   const tasksList = getItem('tasksList') || [];
-  const newTask = tasksList.concat({
+  const newTask = {
     id: Math.random().toString(),
     text,
     done: false,
-  });
-  setItem('tasksList', newTask);
-  //tasks.push(newTask);
+  };
+  tasksList.push(newTask);
+  setItem('tasksList', tasksList);
+
 
   renderTasks()
 }
--- lesson-27-local-storage/task3/src/scripts/render.js
+++ lesson-27-local-storage/task3/src/scripts/render.js
@@ -1,14 +1,13 @@
+import { getItem } from './storage.js';
+
 const listElem = document.querySelector('.list');
+
 const compareTasks = (a, b) => {
   if (a.done !== b.done) {
     return a.done - b.done;
   }
   return a.text.localeCompare(b.text);
-}
-
-const getItem = (key) => {
-  return JSON.parse(localStorage.getItem(key));
-}
+};
 
 const createCheckbox = ({ done, id }) => {
   const checkboxElem = document.createElement("input");
@@ -17,18 +16,18 @@ const createCheckbox = ({ done, id }) => {
   checkboxElem.checked = done;
   checkboxElem.classList.add("list__item-checkbox");
   return checkboxElem;
-}
+};
 
 const createListItem = ({ text, done, id }) => {
   const listItemElem = document.createElement("li");
   listItemElem.classList.add("list__item");
-  const checkboxElem = createCheckbox({ done, id });
   if (done) {
     listItemElem.classList.add("list__item_done");
   }
-  listItemElem.appendChild(checkboxElem, text);
+  const checkboxElem = createCheckbox({ done, id });
+  listItemElem.append(checkboxElem, document.createTextNode(text));
   return listItemElem;
-}
+};
 
 export const renderTasks = () => {
   const tasksList = getItem('tasksList') || [];
@@ -36,5 +35,5 @@ export const renderTasks = () => {
   const tasksElem = tasksList
     .sort(compareTasks)
     .map(createListItem);
-  listElem.append(...tasksElem)
-}
+  listElem.append(...tasksElem);
+};
