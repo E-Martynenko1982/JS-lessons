ea9b866403a52860ef559b0bc121a0c8263b2e48
E-Martynenko1982 <motor8103@gmail.com>
2024-07-11 17:27:29 +0300

    update

---
+++ lesson-30-promises-intro/hw-1/index.js
@@ -0,0 +1,25 @@
+const addImageV2 = () => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement("img");
+    imgElem.setAttribute("alt", "My Photo");
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector(".page");
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+    imgElem.addEventListener('load', onImageLoaded)
+
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')))
+  });
+  return p;
+};
+const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+const resultPromise = addImage(imgSrc);
+
+resultPromise.then(img => {
+  console.log(img);
+}).catch(error => console.log(error)
+)
