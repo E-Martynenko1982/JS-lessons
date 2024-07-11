52ba0a46e98c31c1399fbe561d36d992eb82858a
E-Martynenko1982 <motor8103@gmail.com>
2024-07-11 18:09:39 +0300

    update

--- lesson-30-promises-intro/promisification/index.js
+++ lesson-30-promises-intro/promisification/index.js
@@ -1,6 +1,6 @@
 import { addImage } from "./addImage.js";
 
-const addImageV2 = url => {
+export const addImageV2 = url => {
   return new Promise((resolve, reject) => {
     addImage(url, (error, data) => {
       if (error) {
