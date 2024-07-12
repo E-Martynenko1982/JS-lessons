d4b03c9245e95ec72067ab4116350e8a1dc27776
E-Martynenko1982 <motor8103@gmail.com>
2024-07-12 15:41:09 +0300

    update

--- lesson-31-promise-h&ch/task-2-calc/index.js
+++ lesson-31-promise-h&ch/task-2-calc/index.js
@@ -1,34 +1,23 @@
-export const asyncCalculator = number => {
-  return new Promise((resolve, reject) => {
-    if (number === 5) {
-      setTimeout(() => {
-        console.log(`Initial value: ${number}`);
-        resolve(number);
-      }, 500); 
-    } else {
-      reject(new Error("Number is not 5"));
-    }
-  });
-};
+ export const asyncCalculator = number => new Promise((resolve) => {
+  setTimeout(() => {
+console.log(`Initial value: ${number}`)
+resolve(number)
 
+  }, 500)
+})
+.then(value => new Promise((resolve) => {
+  setTimeout(() => {
+    const result = value * value;
+    console.log(`Squared value: ${result}`)
+    resolve(result)
+  }, 500);
+}))
+.then(value => new Promise((resolve) => {
+  setTimeout(() => {
+    const result = value * 2;
+    console.log(`Squared value: ${result}`)
+    resolve(result)
+  }, 0);
+}))
+ 
 asyncCalculator(5)
-  .then(data => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log(`Squared value: ${data * data}`);
-        resolve(data * data);
-      }, 1000);
-    });
-  })
-  .then(data => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log(`Doubled value: ${data * 2}`);
-        resolve(data * 2);
-      }, 0)
-    });
-  })
-  .catch(error => {
-    console.error(error.message);
-  });
-
