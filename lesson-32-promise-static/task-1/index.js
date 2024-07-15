1481095a5c1917777f13a26114797aa36bff2989
E-Martynenko1982 <motor8103@gmail.com>
2024-07-15 13:44:48 +0300

    update

---
+++ lesson-32-promise-static/task-1/index.js
@@ -0,0 +1,19 @@
+const asyncValueWithDelay = (value, delay) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log(value)
+
+    resolve(value);
+  }, delay)
+})
+
+const sumNumbers = (numbers) => {
+  return numbers
+    .filter(number => !isNaN(number))
+    .reduce((acc, number) => acc + Number(number), 0)
+};
+
+const asyncSum = (...arrNum) => {
+  return Promise.all(arrNum)
+    .then(numbers => sumNumbers(numbers))
+    .catch(() => Promise.reject(new Error('Can\'t calculate')));
+};
