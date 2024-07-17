14c475272b61dbc2c89f604fa10f32b26cbec180
E-Martynenko1982 <motor8103@gmail.com>
2024-07-17 15:28:55 +0300

    update

--- lesson-33-serverConnect/task-2/index.js
+++ lesson-33-serverConnect/task-2/index.js
@@ -15,13 +15,13 @@ export function getTaskById(taskId) {
     .then(response => response)
 }
 
-getTaskById('2')
-  .then(task => console.log(task))
-// examples
-getTasksList().then(tasksList => {
-  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
-});
+// getTaskById('2')
+//   .then(task => console.log(task))
+// // examples
+// getTasksList().then(tasksList => {
+//   console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
 
-getTaskById('2').then(taskData => {
-  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
-});
+// getTaskById('2').then(taskData => {
+//   console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
