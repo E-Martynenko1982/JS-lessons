752ba75e54cd008a76c672896db73ba93cb28475
E-Martynenko1982 <motor8103@gmail.com>
2024-07-22 15:46:31 +0300

    update

---
+++ lesson-34-http-practice/task-3-CRUD/index.js
@@ -0,0 +1,81 @@
+
+// const baseUrl = 'https://6698d3872069c438cd7036bb.mockapi.io/api/V1/users';
+
+// const data = {
+//   email: "cool@email.com",
+//   firstName: "Iron",
+//   lastName: "Man",
+//   age: 42,
+// }
+
+// const userData = {
+//   email: "cool@email.com",
+//   firstName: "Shelby",
+//   lastName: "Dron",
+//   age: 77,
+// }
+
+export function getUsersList() {
+
+  return fetch(baseUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+
+  })
+    .then(response => response.json())
+
+
+}
+
+export function getUserById(userId) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+
+  })
+    .then(response => response.json())
+
+
+}
+
+
+export function createUser(userData) {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(response => response.json())
+
+}
+
+export function deleteUser(userId) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+
+  })
+    .then(response => response.json())
+
+}
+
+export function updateUser(userId, userData) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(response => response.json())
+}
+
+
