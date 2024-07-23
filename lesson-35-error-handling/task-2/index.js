3838ef9a48e6485b077291ab40d4e4870eba3e84
E-Martynenko1982 <motor8103@gmail.com>
2024-07-23 18:58:28 +0300

    update

--- lesson-35-error-handling/task-2/index.js
+++ lesson-35-error-handling/task-2/index.js
@@ -14,7 +14,7 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 failRequest
   .catch(function onError2(error) {
     console.error('onError2', error.message);
-    throw error; // (2)
+    throw new Error("Server error");; // (2)
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data);
