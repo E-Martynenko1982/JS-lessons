268555ae93aa15cdd256d0bcb903a2d4968cf04d
E-Martynenko1982 <motor8103@gmail.com>
2024-07-25 13:50:01 +0300

    update

---
+++ lesson-36-async-await/hw-1/index.js
@@ -0,0 +1,17 @@
+const getUsersBlogs = async (users) => {
+  try {
+    const requests = users.map(user => fetch(`https://api.github.com/users/${user}`).then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch user data');
+      }
+      return response.json();
+    }));
+
+    const usersData = await Promise.all(requests);
+    return usersData.map(userData => userData.blog);
+  } catch (error) {
+    throw new Error('Failed to fetch data for one or more users');
+  }
+};
+
+getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList))
