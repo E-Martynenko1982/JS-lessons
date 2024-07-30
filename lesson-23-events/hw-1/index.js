0e49b090acefee2889fc1c6d9440a435d17071fa
E-Martynenko1982 <motor8103@gmail.com>
2024-07-30 18:39:44 +0300

    update

--- lesson-23-events/hw-1/index.js
+++ lesson-23-events/hw-1/index.js
@@ -45,16 +45,17 @@ const renderTasks = tasksList => {
 
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
-      if (done === true) {
-        listItemElem.classList.add('list__item_done');
-      }
+
       // створення і налаштування чекбокса
       const checkboxElem = document.createElement('input');
       checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.setAttribute('data-id', id);
       checkboxElem.checked = done;
       checkboxElem.dataset.taskId = id;
       checkboxElem.classList.add('list__item-checkbox');
-      checkboxElem.addEventListener('click', onCheckboxClick);
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
       // додавання чекбокса і тексту задачі в елемент списку
       listItemElem.append(checkboxElem, text);
 
@@ -62,6 +63,7 @@ const renderTasks = tasksList => {
     });
   // додає всі створені елементи до списку на сторінці.
   listElem.append(...listItemsElems);
+
 };
 // Ця функція перемикає стан завдання (виконана/невиконана) при натисканні на чекбокс
 const onCheckboxClick = e => {
